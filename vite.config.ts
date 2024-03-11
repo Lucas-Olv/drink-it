4353f429bf32ba7160ebb6ee8591c7197b2a5e3f
Lucas Lopes <lucas.lopes@teknisa.com>
2024-03-11 19:47:13 -0300

    Configurado projeto para ser uma PWA.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,43 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Drink It!',
+        short_name: 'DrinkIt',
+        description: 'Nunca mais se esqueça de beber água, atinja sua meta diária com Drink It!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa_icon_192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa_icon_512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            "src": "mask-icon.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
