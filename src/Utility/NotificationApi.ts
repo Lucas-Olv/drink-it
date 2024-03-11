204844c6d789b50b0d383027c15e5491c6cd090d
Lucas Lopes <lucas.lopes@teknisa.com>
2024-03-11 20:12:49 -0300

    Adição de função para inscrever usuário para notificações.

---
+++ src/Utility/NotificationApi.ts
@@ -0,0 +1,80 @@
+export async function subscribeUserToPushNotifications() {
+  if (!('serviceWorker' in navigator)) {
+    console.warn("Service Worker isn't supported on this browser");
+    return;
+  }
+  
+  if (!('PushManager' in window)) {
+    console.warn("Push isn't supported on this browser");
+    return;
+  }
+  
+  await registerServiceWorker();
+  await askPermission();
+  await subscribeUserToPush();
+}
+
+async function registerServiceWorker() {
+  return navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function (registration) {
+      console.log('Service worker successfully registered.');
+      return registration;
+    })
+    .catch(function (err) {
+      console.error('Unable to register service worker.', err);
+    });
+}
+
+async function askPermission() {
+  return new Promise(function (resolve, reject) {
+    const permissionResult = Notification.requestPermission(function (result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  }).then(function (permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error("We weren't granted permission.");
+    }
+  });
+}
+
+async function subscribeUserToPush() {
+  return navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function (registration) {
+      const subscribeOptions = {
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(
+          `${import.meta.env.VITE_VAPID_KEY}`,
+        ),
+      };
+
+      return registration.pushManager.subscribe(subscribeOptions);
+    })
+    .then(function (pushSubscription) {
+      console.log(
+        'Received PushSubscription: ',
+        JSON.stringify(pushSubscription),
+      );
+      return pushSubscription;
+    });
+}
+
+function urlBase64ToUint8Array(base64String: string) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
