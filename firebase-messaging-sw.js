482d59fc028d2932bc9ddb4341a9b8d38d60e420
Lucas Lopes de Oliveira <81447563+Lucas-Olv@users.noreply.github.com>
2024-03-12 14:41:34 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,14 +9,14 @@ importScripts(
 // import { getMessaging, onMessage, onBackgroundMessage } from "firebase/messaging/sw";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: `${process.env.env.VITE_API_KEY}`,
-  authDomain: `${process.env.VITE_AUTH_DOMAIN}`,
-  databaseURL: `${process.env.VITE_DATABASE_URL}`,
-  projectId: `${process.env.VITE_PROJECT_ID}`,
-  storageBucket: `${process.env.VITE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.VITE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.VITE_APP_ID}`,
-  measurementId: `${process.env.VITE_MEASUREMENT_ID}`
+  apiKey: "<API_KEY>",
+  authDomain: "<AUTH_DOMAIN>",
+  databaseURL: "<DATABASE_URL>",
+  projectId: "<PROJECT_ID>",
+  storageBucket: "<STORAGE_BUCKET>",
+  messagingSenderId: "<MESSAGING_SENDER_ID>",
+  appId: "<APP_ID>",
+  measurementId: "<MEASUREMENT_ID>"
 });
 
 const messaging = firebase.messaging(firebaseConfig);
