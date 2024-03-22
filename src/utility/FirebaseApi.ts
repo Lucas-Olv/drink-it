69836f971787946c0d7e3e1c2c763263e6e7cf6d
Lucas Lopes <lucas.lopes@teknisa.com>
2024-03-22 20:28:31 -0300

    Implementado funcionamento do firebase Auth e Firestore.

--- src/utility/FirebaseApi.ts
+++ src/utility/FirebaseApi.ts
@@ -1,7 +1,8 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Analytics, getAnalytics } from "firebase/analytics";
 import { Messaging, getMessaging, getToken } from "firebase/messaging";
-import { getFirestore, addDoc, collection, Firestore } from "firebase/firestore";
+import { getFirestore, Firestore, setDoc, updateDoc, doc } from "firebase/firestore";
+import { Auth, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default class FirebaseApi {
 
@@ -11,6 +12,8 @@ export default class FirebaseApi {
   private static messaging: Messaging;
   private static vapidKey: {};
   private static db: Firestore;
+  private static auth : Auth;
+  private static authProvider : GoogleAuthProvider;
 
   init() {
 
@@ -24,26 +27,61 @@ export default class FirebaseApi {
       appId: `${import.meta.env.VITE_APP_ID}`,
       measurementId: `${import.meta.env.VITE_MEASUREMENT_ID}`
     };
-    FirebaseApi.vapidKey = {vapidKey: `${import.meta.env.VITE_VAPID_KEY}`};
+    FirebaseApi.vapidKey = { vapidKey: `${import.meta.env.VITE_VAPID_KEY}` };
 
     FirebaseApi.app = initializeApp(FirebaseApi.firebaseConfig);
     FirebaseApi.analytics = getAnalytics(FirebaseApi.app);
     FirebaseApi.messaging = getMessaging(FirebaseApi.app);
     FirebaseApi.db = getFirestore(FirebaseApi.app);
+    FirebaseApi.auth = getAuth(FirebaseApi.app);
+    FirebaseApi.authProvider = new GoogleAuthProvider();
   }
-  
+
   async requestUserPermissions() {
+    const userUid = localStorage.getItem('userUid') || '';
     const permissionResult = await Notification.requestPermission();
-  
+
     if (permissionResult === "granted") {
       const firebaseToken = await getToken(FirebaseApi.messaging, FirebaseApi.vapidKey);
+
       if (firebaseToken) {
-        return firebaseToken;
+        const docReference = doc(FirebaseApi.db, 'authenticated-users', userUid);
+        await updateDoc(docReference, {firebaseMessagingToken: firebaseToken});
       }
     } else {
       console.warn("User denied permission for notifications.");
     }
-  
+  }
+
+  logInWithGoogleProvider() {
+    signInWithPopup(FirebaseApi.auth, FirebaseApi.authProvider)
+  .then(async (result) => {
+    const user = result.user;
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    const docReference = doc(FirebaseApi.db, 'authenticated-users', user.uid);
+
+    localStorage.setItem('userUid', user.uid);
+
+    await setDoc(docReference, {
+      name: user.displayName,
+      email: user.email,
+      firebaseMessagingToken: "",
+      notificationInterval: "",
+      dailyIntakeGoal: "",
+      accessToken: token
+    });
+
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
   }
 }
 
